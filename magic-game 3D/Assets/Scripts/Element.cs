8303846432b18460a089e16aeef0847b33db1632
moliko <albertobkn90@gmail.com>
2018-04-29 15:34:50 -0300

    proyect unity folder

---
+++ magic-game 3D/Assets/Scripts/Element.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Element : MonoBehaviour {
+
+    public List<Tipos> Elementos;
+    
+
+    void Update()
+    {
+        foreach(Tipos t in Elementos)
+        {
+           if(t.xp >= 100)
+            {
+                t.xp = 0;
+                t.Lvl = t.Lvl+ 1; 
+            } 
+        }
+    }
+
+}
+        [System.Serializable]
+        public class Tipos
+{
+    public string Basicos;
+    public bool aprendido;
+    public Color color; [Range(0,1)]public float emision;
+    public Material material;
+
+    public float xp;
+
+    public int price;
+    public int Lvl;
+    public int minlvl;
+    public Button boton; 
+
+    public void Ecolor() {
+            
+    }
+}
---
+++ magic-game 3D/Assets/Scripts/ItemType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemType : ScriptableObject {
+
+    public int ID;
+    public float _Cost;
+    public GameObject _Object;
+    
+}
