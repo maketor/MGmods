8add7e08e1a99e23321fc0f0a780b8202d9db5c1
moliko <albertobkn90@gmail.com>
2018-05-03 08:03:33 -0300

    hiii

--- .magic-game3D/Assets/Scripts/Os.cs
+++ .magic-game3D/Assets/Scripts/Os.cs
@@ -9,11 +9,18 @@ public class Os : MonoBehaviour {
         Debug.Log(SystemInfo.batteryStatus);//muestra si esta cargando o esta desconectado
         Debug.Log(SystemInfo.systemMemorySize); //memoria de ram           int
         Debug.Log(SystemInfo.graphicsMemorySize + " mb " + SystemInfo.graphicsDeviceName);//memoria de grafica.. int
+
+        Debug.Log("<color=blue>Pantallas conectadas: </color><color=red>" + Display.displays.Length + "</color>");
+        if (Display.displays.Length > 1)
+            Display.displays[1].Activate();
+        if (Display.displays.Length > 2)
+            Display.displays[2].Activate();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Handheld.Vibrate();
+        //Handheld.Vibrate();                   //testin vibration on the cellphone android and ios..
 	}
     void OnGUI()
     {
---
+++ .magic-game3D/Assets/Shaders/MiShader.shader
@@ -0,0 +1,29 @@
+﻿shader "nice/miShader"{
+	SubShader
+	{
+		pass 
+		{
+			tags{"RenderType"="Opaque"}
+
+				CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.Cginc"
+				struct v2f {
+				float4 pos : SV_POSITION;
+};
+			v2f vert(appdata.base v) {
+				v2f o;
+				float4 vert : float4(v.vertex.xyz * sin(_Time.y), v.vertex.w);
+
+					o.pos = nul(UNITY_MATRIX_MVP, vert);
+					return o;
+			}
+			half4 frag(v2f i) :COLOR{
+				return half4(1,1,1,1);
+
+			}
+				ENDCG
+		}
+	}
+}
